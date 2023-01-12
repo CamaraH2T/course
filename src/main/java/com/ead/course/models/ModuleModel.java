cc6cd4eac1102f48eec0e850d8d8180293c755ea
Heittor Migliacio <heittor.migliacio@seniorsolution.com.br>
2023-01-12 06:23:33 -0300

    Implementing JPA

--- src/main/java/com/ead/course/models/ModuleModel.java
+++ src/main/java/com/ead/course/models/ModuleModel.java
@@ -2,18 +2,20 @@ package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_MODULES")
-public class ModuleModels implements Serializable {
+public class ModuleModel implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,4 +27,10 @@ public class ModuleModels implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
+    @ManyToOne(optional = false)
+    private CourseModel course;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "module")
+    private Set<LessonModel> lessons;
 }
--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,9 +1,9 @@
 package com.ead.course.repositories;
 
-import com.ead.course.models.ModuleModels;
+import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ModuleRepository extends JpaRepository<ModuleModels, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
 }
